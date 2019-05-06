c7bcf172a6037989f9dbd26465a9c265670b1f8f
yuribreion <yuribreion@br.ibm.com>
2019-05-05 21:06:53 -0300

    Melhorando codigo

--- app/src/main/java/br/com/fiap/nac03/CarrosEconomicosActivity.java
+++ app/src/main/java/br/com/fiap/nac03/CarrosEconomicosActivity.java
@@ -1,8 +1,10 @@
 package br.com.fiap.nac03;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class CarrosEconomicosActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class CarrosEconomicosActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_economicos);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Carros economicos");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, MainActivity.class));
+                finishAffinity();
+                break;
+            default:break;
+        }
+        return true;
     }
 }
